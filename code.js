9d371eac5143e05bea07b0f1428087ce76b60a17
Aidan Giraldo <agirald1@uwyo.edu>
2023-12-11 21:07:02 -0700

    code finished, moving onto making automated testing

--- code.js
+++ code.js
@@ -1,3 +1,39 @@
-function hasCycle(graph) {
+function hasCycle(graph, start = 0, visited = []) 
+{
+    // To detect a cycle we iterate over all edges in a graph
+    // and if we run into a previously visited vertex, then we
+    // have detected a cycle. 
+
+    // We mark the vertex we're on as visited
+    visited[start] = true; 
+
+    // We now start iteratiing through the adjacency matrix and 
+    // following the edges 
+    for (var i = 0; i < graph[start].length; i++)
+    {
+        // initialize next vertex to visit
+        vertex = graph[start][i]; 
+
+        // if the vertex hasn't been visited, visit 
+        if (visited[vertex] != true )
+        {
+            return hasCycle(graph, vertex, visited); 
+        }
+        
+        // if it has been visited, we've detected a cycle
+        else 
+        {
+            return true; 
+        }
+    }
     return false;
 }
+
+var graph = [
+    [1],
+    [2],
+    [3],
+    []
+];
+
+console.log(hasCycle(graph, 0, []));
